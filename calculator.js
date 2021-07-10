ae48dc945bfdaa136785125665ed274827d6411c
RosmerySI <85293230+RosmerySI@users.noreply.github.com>
2021-07-10 09:03:32 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,26 +1,46 @@
 let screen = document.getElementById('screen-result');
 
 function getData(ref){
-    let value = ref.value;
+
+    let value = ref.value;    
     screen.value += value;
 }
 
 function clean(){
     screen.value = '';
+   
 }
 
 function calculate(){
     try {
-        screen.value = eval(screen.value);
+        let prueba = eval(screen.value);
+		if(isNaN(prueba)){
+			let gif= document.getElementById("gif")
+		}else{
+			screen.value = eval(screen.value);
+		}		
+		
     } catch (error) {
-        screen.value = 'Error';
+	   screen.className="hide";
+       let gif= document.getElementById("gif")
+	   gif.className="show"
         setTimeout(() => {
-            clean();
-        }, 500);
+        clean();
+        }, 100);
+		
     }
+
 }
 
+function sqrt(){
+    
+    screen.value = Math.sqrt(screen.value);
 
+}
+
+function percent(){
+	screen.value=  (screen.value)
+}
 
 let buttonChange = document.getElementById('switch_label');
 
